245f6300f8c756acb8d5cd21b89e34c8270210f9
Garima Goyat <garimagoyat81688@gmail.com>
2025-04-18 11:40:31 +0530

    Revert "added login/signup page"
    This reverts commit bee625ff913856399715dad17cd24d8646649de3.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage";
-import SignUpPage from "./components/SignUpPage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import ShopOwnerDashboard from './components/ShopOwnerDashboard';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/shop-owner" element={<ShopOwnerDashboard />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
